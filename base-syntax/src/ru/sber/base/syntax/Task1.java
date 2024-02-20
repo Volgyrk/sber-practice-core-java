9677c84dc1a00cc134d4c7352f7f4f42cf43db29
095238 <misha.kruglov35@gmail.com>
2024-02-20 10:46:01 +0300

    выполнено первое задание

--- base-syntax/src/ru/sber/base/syntax/Task1.java
+++ base-syntax/src/ru/sber/base/syntax/Task1.java
@@ -2,6 +2,6 @@ package ru.sber.base.syntax;
 
 public class Task1 {
     public static void main(String[] args) {
-        System.out.print(7 14 21 28 35 42 49 56 63 70 77 84 91 98);
+        System.out.print("7 14 21 28 35 42 49 56 63 70 77 84 91 98");
     }
 }
